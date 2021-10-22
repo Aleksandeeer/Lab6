b4968f3c2d696628bfc2dc73d6eb9fa571fd47bf
Александр <90092786+Aleksandeeer@users.noreply.github.com>
2021-10-22 10:19:49 +0300

    Add files via upload

---
+++ 9.10.21/ClassTasks9.10.21/Class.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+
+namespace ClassTasks9._10._21
+{
+    class Class
+    {
+        struct Student
+        {
+            public string name;
+            public string surname;
+            public ushort YearOfBirth;
+            public string subject;
+            public ushort scores;
+        }
+
+        private const string PathToListStudent = @"C:\\Users\\Huawei\\source\\repos\\ClassTasks9.10.21\\Students.txt";
+        static void Main()
+        {
+            //Console.WriteLine("Task 1");
+            //int count;
+            //Console.Write("Введите количество викингов в каждой команде:");
+            //count = Convert.ToInt32(Console.ReadLine());
+            //int[] BavarianBeerBears = new int[count];
+            //int[] ScandinavianSchollers = new int[count];
+            //for (int i = 0; i < count; i++)
+            //{
+            //    Console.Write($"{i + 1} викинг из \"Bavarian Beer Bears\" показал: ");
+            //    BavarianBeerBears[i] = Convert.ToByte(Console.ReadLine());
+            //}
+            //for (int i = 0; i < count; i++)
+            //{
+            //    Console.Write($"{i + 1} викинг из \"Scandinavian Schollers\" показал: ");
+            //    ScandinavianSchollers[i] = Convert.ToByte(Console.ReadLine());
+            //}
+            //var BavariansFive = BavarianBeerBears.Count(x => x == 5);
+            //Console.WriteLine("Количество пятёрок в \"Bavarian Beer Bear\": " + BavariansFive);
+            //var SchollersFive = ScandinavianSchollers.Count(x => x == 5);
+            //Console.WriteLine("Количество пятёрок в \"Bavarian Beer Bear\": " + SchollersFive);
+            //if (BavariansFive == SchollersFive)
+            //{
+            //    Console.WriteLine("Drinks All Round! Free Beers on Bjorg!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Ой, Бьорг - пончик! Ни для кого пива!");
+            //}
+
+            //Console.WriteLine("\nTask 2");
+            //List<int> Pictures = new List<int>();
+            //string Numberfromfile; int numberofstring = 0;
+            //StreamReader sr = new StreamReader("C:\\Users\\Huawei\\source\\repos\\ClassTasks9.10.21\\Numbers.txt");
+            //while ((Numberfromfile = sr.ReadLine()) != null)
+            //{
+            //    Pictures[numberofstring] = Convert.ToInt32(Numberfromfile);
+            //    numberofstring++;
+            //}
+            //Random rnd = new Random();
+            //foreach (int numb in Pictures)
+            //{
+            //    int random = rnd.Next(0, 63);
+            //    int buffer = Pictures[random];
+            //    Pictures[random] = Pictures[numb];
+            //    Pictures[numb] = buffer;
+            //    Console.WriteLine(Pictures[numb] + ", ");
+            //}
+
+            Console.WriteLine("\nTask 3");
+            string stringfromfile; int numberString = 1;
+            Dictionary<string, Student> studentDictionary = new Dictionary<string, Student>();
+            StreamReader fileTextRead = new StreamReader(PathToListStudent);
+                while ((stringfromfile = fileTextRead.ReadLine()) != null) //считывание файла построчно пока не кончатся символы
+                {
+                    string[] dateStudent = stringfromfile.Split();
+                    Student studentNew; //создание нового студента
+                    studentNew.surname = dateStudent[0];
+                    studentNew.name = dateStudent[1];
+                    studentNew.YearOfBirth = Convert.ToUInt16(dateStudent[2]);
+                    studentNew.subject = dateStudent[3];
+                    studentNew.scores = Convert.ToUInt16(dateStudent[4]);
+                    studentDictionary.Add(dateStudent[0] + " " + dateStudent[1], studentNew);
+                    numberString++; //переход на следующую строку
+                }
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("* * * Меню * * *\n1.Добавить нового студента\n2.Удалить студента по имени и фамилии\n3. Сортировать по баллам");
+                Console.Write("Выберите действие. Нажми цифру: ");
+                byte choiсe = Convert.ToByte(Console.ReadLine());
+                Console.WriteLine();
+                switch (choiсe)
+                {
+                    case 1:
+                        Student studentNew;
+                        Console.Write("Введите имя: ");
+                        studentNew.name = Console.ReadLine();
+                        Console.Write("Введите фамилию студента: ");
+                        studentNew.surname = Console.ReadLine();
+                        Console.Write("Введите год рождения: ");
+                        studentNew.YearOfBirth = Convert.ToUInt16(Console.ReadLine());
+                        Console.Write("Введите основной предмет: ");
+                        studentNew.subject = Console.ReadLine();
+                        Console.Write("Введите количество баллов: ");
+                        studentNew.scores = Convert.ToUInt16(Console.ReadLine());
+                        studentDictionary.Add(studentNew.name + " " + studentNew.surname, studentNew);
+                        break;
+                    case 2:
+                        Console.Write("Введите имя и фамилию студента: ");
+                        string studentRemove = Console.ReadLine();
+                        if (!studentDictionary.Remove(studentRemove))
+                        {
+                            Console.WriteLine("Ошибка: такого студента не существует");
+                        }
+                        break;
+                    case 3:
+                       // studentDictionary = studentDictionary.OrderBy(item => item.Value.scores).ToDictionary(item => item.Key, item => item.Value);
+                        List<Student> studentsList = new List<Student>();
+                        foreach (var item in studentDictionary)
+                        {
+                            studentsList.Add(item.Value);
+                        }
+                        foreach (var item in QuickSortListStruct(studentsList))
+                        {
+                            Console.WriteLine(item.name + " " + item.surname + " "
+                                + item.YearOfBirth + " " + item.subject + " " + item.scores);
+                        }
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
+
+            Console.WriteLine("\nTask 4");
+            Console.WriteLine("\nTask 5"); //не сделал оба
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+        
+        static List<Student> QuickSortListStruct(List<Student> list)
+        {
+            int left = 0, right = list.Count() - 1;
+            Student a, b;
+            if (left >= right)
+            {
+                return list;
+            }
+            var fundation = left - 1;
+            for (var i = left; i < right; i++)
+            {
+                if (list[i].scores < list[right].scores)
+                {
+                    fundation++;
+                    a = list[fundation];
+                    b = list[i];
+                    Swap(ref a, ref b);
+                }
+            }
+            return list;
+        }
+        static List<Student> QuickSortListStruct(List<Student> list, int left, int right)
+        {
+            Student a, b;
+            if (left >= right)
+            {
+                return list;
+            }
+            var fundation = left - 1;
+            for (var i = left; i < right; i++)
+            {
+                if (list[i].scores < list[right].scores)
+                {
+                    fundation++;
+                    a = list[fundation];
+                    b = list[i];
+                    Swap(ref a, ref b);
+                }
+            }
+
+            fundation++;
+            a = list[fundation];
+            b = list[right];
+            Swap(ref a, ref b);
+            QuickSortListStruct(list, left, fundation - 1);
+            QuickSortListStruct(list, fundation + 1, right);
+
+            return list;
+        }
+        static void Swap(ref Student x, ref Student y)
+        {
+            var t = x;
+            x = y;
+            y = t;
+        }
+    }
+}
---
+++ 9.10.21/Laboratory Work №6/Laboratory.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Laboratory_Work__6
+{
+    class Laboratory
+    {
+        static void Main()
+        {
+            Console.WriteLine("Task 6.1");
+            string array = (File.ReadAllText("C:\\Users\\Huawei\\source\\repos\\ClassTasks9.10.21\\Буквы.txt"));
+            Console.WriteLine($"Количество гласных: {GetCountVowels(array)}\nКоличество согласных: {GetCountConsonans(array)}");
+
+            Console.WriteLine("\nTask 6.2");
+            LinkedList<int> matrix1 = new LinkedList<int>();
+            LinkedList<int> matrix2 = new LinkedList<int>();
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine($"Введите a({i + 1},{j + 1}) для 1ой и 2ой матрицы:");
+                    matrix1.AddLast(Convert.ToInt32(Console.ReadLine()));
+                    matrix2.AddLast(Convert.ToInt32(Console.ReadLine()));
+
+                }
+            }
+            Console.WriteLine("1 - печать матриц\n2 - умножение матриц\nВыберите: ");
+            try
+            {
+            start:
+                int todo = Convert.ToInt32(Console.ReadLine());
+                if (todo == 1)
+                {
+                    Press(matrix1, matrix2);
+                }
+                else if (todo == 2)
+                {
+                    Multiplication(matrix1, matrix2);
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: введеное неверное число. Повторите ввод: ");
+                    goto start;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: введён неверный формат.");
+            }
+
+            Console.WriteLine("\nTask 6.3");
+            Random rnd = new Random();
+            Dictionary<int, double> months = new Dictionary<int, double>(12);
+            double sum; int numberofmonth = 0;
+            double[] temperature = new double[30];
+            for (int m = 0; m < 12; m++) //заполнение месяцей и дней температурами
+            {
+                sum = 0;
+                for (int i = 0; i < 30; i++)
+                {
+                    temperature[i] = rnd.Next(120, 300)/10;
+                    sum += temperature[i];
+                }
+                AverageTemperature(numberofmonth, sum, months); //метод для нахождения средних температур в каждом месяце
+                numberofmonth++; //прибавление для метода
+            }
+            for (int k = 0; k < 12; k++) //сортировка массива средних температур
+            {
+                for (int l = 0; l < 12; l++)
+                {
+                    if (months[k] < months[l])
+                    {
+                        double buffer = months[k];
+                        months[k] = months[l];
+                        months[l] = buffer;
+                    }
+                }
+            }
+            Console.Write("Отсортированный массив: ");
+            foreach(var celsium in months)
+                Console.Write(celsium.Value + ", ");
+
+            Console.ReadKey();
+        }
+        static void Press(LinkedList<int> Array1, LinkedList<int> Array2)
+        {
+            Console.WriteLine("1ая матрица:");
+            for (int i = 0; i < 4; i++)
+            {
+               Console.Write(Array1.First() + " ");
+               if (i == 1 || i == 3)
+               {
+                   Console.WriteLine();
+               }
+               Array1.RemoveFirst();
+            }
+            Console.WriteLine("2ая матрица:");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write(Array2.First() + " ");
+                if (i == 1)
+                {
+                    Console.WriteLine();
+                }
+                Array2.RemoveFirst();
+            }
+        }
+        static void Multiplication(LinkedList<int> matrix1, LinkedList<int> matrix2)
+        {
+            int a11, a12, a21, a22;
+            a11 = matrix1.ElementAt(0) * matrix2.ElementAt(0) + matrix1.ElementAt(1) * matrix2.ElementAt(2);
+            a12 = matrix1.ElementAt(0) * matrix2.ElementAt(1) + matrix1.ElementAt(1) * matrix2.ElementAt(3);
+            a21 = matrix1.ElementAt(2) * matrix2.ElementAt(0) + matrix1.ElementAt(3) * matrix2.ElementAt(2);
+            a22 = matrix1.ElementAt(2) * matrix2.ElementAt(1) + matrix1.ElementAt(3) * matrix2.ElementAt(3);
+            Console.WriteLine(a11 + " " + a12 + "\n" + a21 + " " + a22);
+        }
+        static void AverageTemperature(int numberofmonth, double sum, Dictionary<int, double> months)
+        {
+            months[numberofmonth] = Math.Round(sum / 30, 2);
+            Console.WriteLine($"в {numberofmonth + 1} месяце: " + months[numberofmonth]);
+        }
+        static int GetCountVowels (string array)
+        {
+            int lowelscount = 0;
+            List<char> vowels = new List<char>() { 'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я' };
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (array[i] == vowels[j])
+                    {
+                        lowelscount++;
+                    }
+                }
+            }
+            return lowelscount;
+        }
+        static int GetCountConsonans (string array)
+        {
+            int consonanscount = 0;
+            List<char> consonans = new List<char>() { 'б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ъ' };
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < 23; j++)
+                {
+                    if (array[i] == consonans[j])
+                    {
+                        consonanscount++;
+                    }
+                }
+            }
+            return consonanscount;
+        }
+    }
+}
